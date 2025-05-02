21a8577184163404a47b67ab9c1ea48e6c661c3b
Dylaris <3445818679@qq.com>
2025-05-02 23:15:21 +0800

    add luabc build

--- build.lua
+++ build.lua
@@ -1,38 +1,49 @@
 local luabc = require("luabc")
-local cmd   = luabc.cmd:new()
+local cmd   = luabc.cmd
 local tool  = luabc.tool
+local debug = luabc.debug
 
--- gcc -Wall -Wextra -std=gnu11 -I src/ -o dysh src/main.c src/parse.c
+local CC            = "gcc"
+local CFLAGS        = { "-Wall", "-Wextra", "-I src/" }
+local CSTD          = "-std=gnu11"
 
-local CC     = "gcc"
-local CFLAGS = { "-Wall", "-Wextra", "-I src/" }
-local CSTD   = "-std=gnu11"
-local TARGET = "dysh"
-local SRC    = { "src/parse.c", "src/main.c" }
-local OBJ    = (function () 
-    local res = {}
-    for _, file in ipairs(SRC) do
-        table.insert(res, tool.replace_file_extension(file, "o"))
+local TARGET        = "dysh"
+local DYSH_SRC      = tool.match_file_extension(".c", "src")
+local DYSH_OBJ      = tool.replace_files_extension(DYSH_SRC, ".o")
+local DYSH_CLEAN    = { TARGET, table.unpack(DYSH_OBJ) }
+
+local CMD_SRC       = tool.match_file_extension(".c", "src/builtin")
+local CMD_CLEAN     = "src/bin/"
+local CMD_TARGET    = tool.replace_paths_directory(tool.replace_files_extension(CMD_SRC, ""), "src/bin/")
+
+local function build_builtin_cmd()
+    local mkdir = cmd:new()
+    mkdir:append("mkdir -p", "src/bin/")
+
+    for i = 1, #CMD_SRC do
+        local all = cmd:new()
+        all:append(CC, CFLAGS, CSTD, "-o", CMD_TARGET[i], CMD_SRC[i])
     end
-    return res
-end)()
-local CLEAR_FILES = { TARGET, table.unpack(OBJ) }
 
-local function build_obj()
-    table.insert(CFLAGS, "-c")
-    for i = 1, #SRC do
-        cmd:append(CC, CFLAGS, CSTD, "-o", OBJ[i], SRC[i])
-        cmd:print(true)
-        cmd:run()
-    end
-    table.remove(CFLAGS)
+    local clean = cmd:new("clean")
+    clean:append("rm -rf", CMD_CLEAN)
 end
 
 local function build_dysh()
-    build_obj()
-    cmd:append(CC, CFLAGS, CSTD, "-o", TARGET, OBJ)
-    cmd:run()
-    tool.clean(CLEAR_FILES)
+    for i = 1, #DYSH_SRC do
+        local compile = cmd:new()
+        compile:append(CC, CFLAGS, CSTD, "-c", "-o", DYSH_OBJ[i], DYSH_SRC[i])
+    end
+
+    local link = cmd:new()
+    link:append(CC, CFLAGS, CSTD, "-o", TARGET, DYSH_OBJ)
+
+    local clean = cmd:new("clean")
+    clean:append("rm -rf", DYSH_CLEAN)
+
+    build_builtin_cmd()
+
+    luabc.build()
 end
 
 build_dysh()
